61a8fcb0e90b8bed3adcdde91750157f3fa6a64e
Ryan potato pants <ryanja567@gmail.com>
2017-10-09 10:45:20 -0700

    move over

--- src/routes/Profile/components/ProfileView/ProfileView.js
+++ src/routes/Profile/components/ProfileView/ProfileView.js
@@ -5,5 +5,3 @@ import classes from './ProfileView.scss'
 
 export const ProfileView = () => (
     <div className={classes.container} ></div>
-)
-
--- src/routes/Profile/containers/ProfileContainer.js
+++ src/routes/Profile/containers/ProfileContainer.js
@@ -37,17 +37,16 @@ component as well as a <WorkExp /> and <Education />*/
             </Paper>
 
             <Paper className={classes.pane} >
-              <h2>Work Expierence</h2>
-              <p>JobTitle / Company</p>
-              <p>Date Empolyed</p>
+
               <p>Job Description:</p>
             </Paper>
 
             <Paper className={classes.pane} >
-              <h2>Education</h2>
+              <h2>Education:</h2>
               <p>School Name:</p>
-              <p>Date Attended</p>
-              <p>Degree</p>
+              <p>Date Attended:</p>
+              <p>Degree:</p>
+
             </Paper>
             
           </div>  
